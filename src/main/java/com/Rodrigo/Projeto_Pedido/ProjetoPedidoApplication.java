dac48ced2968c0c09b6c7488333507bfdaa99b9c
Rodrigo Marnon <rmarnon@yahoo.com.br>
2019-10-31 08:27:11 -0300

    Projeto Criado

---
+++ src/main/java/com/Rodrigo/Projeto_Pedido/ProjetoPedidoApplication.java
@@ -0,0 +1,13 @@
+package com.Rodrigo.Projeto_Pedido;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoPedidoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoPedidoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Rodrigo/Projeto_Pedido/ProjetoPedidoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Rodrigo.Projeto_Pedido;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoPedidoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
