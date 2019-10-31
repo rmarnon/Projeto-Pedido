43056b062c9b3a344aa7650ededb94ee2e6cf16d
Rodrigo Marnon <rmarnon@yahoo.com.br>
2019-10-31 11:12:39 -0300

    Testando Classe Categoria

--- src/main/java/com/Rodrigo/Projeto_Pedido/resources/CategoriaResource.java
+++ src/main/java/com/Rodrigo/Projeto_Pedido/resources/CategoriaResource.java
@@ -1,16 +1,30 @@
 package com.Rodrigo.Projeto_Pedido.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.Rodrigo.Projeto_Pedido.domain.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "Rest funcionando";
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1, "Informatica");
+		Categoria cat2 = new Categoria(2, "Escritorio");
+		
+		List<Categoria> list = new ArrayList<>();
+		
+		list.add(cat1);
+		list.add(cat2);
+		
+		return list;
 	}
 	
 }
