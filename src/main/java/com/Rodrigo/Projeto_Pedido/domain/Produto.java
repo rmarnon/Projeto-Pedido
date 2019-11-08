f1e4c041e829e7cb69e44de21d40f3eecf3d77b1
Rodrigo Marnon <rmarnon@yahoo.com.br>
2019-11-08 11:29:59 -0300

    Endpoint /pedidos/{id}  disponibilizado

--- src/main/java/com/Rodrigo/Projeto_Pedido/domain/Produto.java
+++ src/main/java/com/Rodrigo/Projeto_Pedido/domain/Produto.java
@@ -16,6 +16,7 @@ import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 public class Produto implements Serializable {
@@ -35,17 +36,10 @@ public class Produto implements Serializable {
 	)
 	private List<Categoria> categorias = new ArrayList<>();
 
+	@JsonIgnore
 	@OneToMany(mappedBy = "id.produto")
 	private Set<ItemPedido> itens = new HashSet<>();
-	
-	public List<Pedido> getPedidos() {
-		List<Pedido> lista = new ArrayList<>();
-		for(ItemPedido x : itens) {
-			lista.add(x.getPedido());
-		}
-		return lista;
-	}
-	
+			
 	public Produto() {
 	}
 
@@ -55,6 +49,15 @@ public class Produto implements Serializable {
 		this.preco = preco;
 	}
 
+	@JsonIgnore
+	public List<Pedido> getPedidos() {
+		List<Pedido> lista = new ArrayList<>();
+		for(ItemPedido x : itens) {
+			lista.add(x.getPedido());
+		}
+		return lista;
+	}
+	
 	public Integer getId() {
 		return id;
 	}
